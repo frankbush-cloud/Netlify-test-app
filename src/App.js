d3c19ffce91b3fe51feaf613872e177f1534073d
frankbush-cloud <frankbush979@gmail.com>
2023-05-22 01:45:32 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import motorBike from "./Images/motorBike.jpg"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I Am Franklin Bush the coder</h1>
+      <img src={motorBike} alt="motorbike"  />
+    </div>
+  );
+}
+
+export default App;
